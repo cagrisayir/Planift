a3443558e4e322252cd027301bf20c641703b67d
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-17 19:15:28 +0300

    Initial Commit

---
+++ Planift/PlaniftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaniftApp.swift
+//  Planift
+//
+//  Created by Omer Cagri Sayir on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaniftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
