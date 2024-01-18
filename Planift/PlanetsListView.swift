51dd99e6ba81c4aaa8fbfe363f28c15cc3d73db1
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-18 16:20:58 +0300

    Fetched data from json

--- Planift/PlanetsListView.swift
+++ Planift/PlanetsListView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct PlanetsListView: View {
+    var planets: [Planet] = Bundle.main.decode("data.json")
+
     var body: some View {
         ZStack {
             Color("mydark")
                 .ignoresSafeArea()
             Image("background-stars")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+
+            VStack {
+                ForEach(planets, id: \.name) { planet in
+                    Text("\(planet.name)").foregroundStyle(.white)
+                }
+            }
         }
-        .padding()
     }
 }
 
