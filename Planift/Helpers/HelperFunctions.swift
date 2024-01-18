b29363b09ef8780eddf3d524a0cdcd965fe3526a
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-18 15:38:22 +0300

    Created Planet Model

---
+++ Planift/Helpers/HelperFunctions.swift
@@ -0,0 +1,14 @@
+//
+//  ImageNameHelper.swift
+//  Planift
+//
+//  Created by Omer Cagri Sayir on 18.01.2024.
+//
+
+import Foundation
+
+extension String {
+    static func removeAssetPath() -> String {
+        return self.replacingOccurrences(<#T##self: String##String#>)
+    }
+}
---
+++ Planift/Models/Planet.swift
@@ -0,0 +1,32 @@
+//
+//  Planet.swift
+//  Planift
+//
+//  Created by Omer Cagri Sayir on 18.01.2024.
+//
+
+import Foundation
+
+struct Planet: Codable, Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var overview: Subcontent
+    var structure: Subcontent
+    var geology: Subcontent
+    var rotation: String
+    var revolution: String
+    var radius: String
+    var temperature: String
+    var images: Images
+
+    struct Subcontent: Codable {
+        var content: String
+        var source: String
+    }
+
+    struct Images: Codable {
+        var planet: String
+        var `internal`: String
+        var geology: String
+    }
+}
